6753b2d581e8e3b93b91ecc0b0a88da22a5934ae
Anwesh43 <anweshthecool0@gmail.com>
2018-06-20 10:57:47 +0530

    adding handler on touch down

--- linkedhexcircleview/src/main/java/com/anwesh/uiprojects/linkedhexcircleview/LinkedHexCircleView.kt
+++ linkedhexcircleview/src/main/java/com/anwesh/uiprojects/linkedhexcircleview/LinkedHexCircleView.kt
@@ -18,6 +18,11 @@ class LinkedHexCircleView(ctx : Context) : View(ctx) {
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
         return true
     }
 }
