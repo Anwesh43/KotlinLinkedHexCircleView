f259264c3044a22e93937e09b0e3b50e568b3cae
Anwesh43 <anweshthecool0@gmail.com>
2018-06-20 11:13:00 +0530

    adding HCNode class

--- linkedhexcircleview/src/main/java/com/anwesh/uiprojects/linkedhexcircleview/LinkedHexCircleView.kt
+++ linkedhexcircleview/src/main/java/com/anwesh/uiprojects/linkedhexcircleview/LinkedHexCircleView.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.content.Context
 import android.graphics.*
 
+val HC_NODES : Int = 5
+
 class LinkedHexCircleView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -74,4 +76,57 @@ class LinkedHexCircleView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class HCNode (var i : Int) {
+
+        private var next : HCNode? = null
+
+        private var prev : HCNode? = null
+
+        private val state : State = State()
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < HC_NODES - 1) {
+                next = HCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val gap : Float = 360f / HC_NODES
+            val r : Float = Math.min(w, h) / 3
+            prev?.draw(canvas, paint)
+            paint.color = Color.parseColor("#e67e22")
+            canvas.save()
+            canvas.translate(w/2, h/2)
+            canvas.rotate(gap * i + gap * state.scale)
+            canvas.drawCircle(r, 0f, r/10, paint)
+            canvas.restore()
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HCNode {
+            var curr : HCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
