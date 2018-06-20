1edbcca25428226f0fe2eeb5777b803a0f19d256
Anwesh43 <anweshthecool0@gmail.com>
2018-06-20 11:26:01 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedhexcircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedhexcircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedhexcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedhexcircleview.LinkedHexCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedHexCircleView.create(this)
     }
 }
