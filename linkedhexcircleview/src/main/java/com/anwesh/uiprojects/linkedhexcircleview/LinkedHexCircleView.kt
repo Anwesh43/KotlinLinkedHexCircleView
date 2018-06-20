0710d8bbd3dd61d2c5d869bdb2e449c28893b272
Anwesh43 <anweshthecool0@gmail.com>
2018-06-20 11:15:20 +0530

    adding LinkedHexCircle class

--- linkedhexcircleview/src/main/java/com/anwesh/uiprojects/linkedhexcircleview/LinkedHexCircleView.kt
+++ linkedhexcircleview/src/main/java/com/anwesh/uiprojects/linkedhexcircleview/LinkedHexCircleView.kt
@@ -129,4 +129,29 @@ class LinkedHexCircleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedHexCircle (var i : Int) {
+
+        private var curr : HCNode = HCNode(i + 1)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(it)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+
+    }
 }
